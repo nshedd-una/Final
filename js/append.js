7b0925a51f79a4a31bf4ebefabb0c1ff433d51e1
nshedd-una <60400986+nshedd-una@users.noreply.github.com>
2020-05-03 21:35:03 -0500

    Add files via upload

---
+++ js/append.js
@@ -0,0 +1,5 @@
+
+//*Applies data to BS cards*//
+$("#card1").append("<p>Course Description<br>A fundamental course in Web Development utilizing contemporary standards, methods, and<br>tools for students to develop skills in creating and deploying web-based content. Students<br>learn development concepts spanning markup and programming languages and libraries,<br>local and hosted environments, versioning control systems, and development tools.<br>...</p>");
+
+$("#card2").append("<p>Course Description<br>A fundamental course in Web Development utilizing contemporary standards, methods, and<br>tools for students to develop skills in creating and deploying web-based content. Students<br>learn development concepts spanning markup and programming languages and libraries,<br>local and hosted environments, versioning control systems, and development tools.<br>...</p>");
